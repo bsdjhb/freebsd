f1667b829bc873bf2f1ff55e4084a01c3bc81a53
Roland Dreier <rolandd@cisco.com>
2008-03-12 07:51:03 -0700

    IPoIB: Allocate priv->tx_ring with vmalloc()
    Commit 7143740d ("IPoIB: Add send gather support") made struct
    ipoib_tx_buf significantly larger, since the mapping member changed
    from a single u64 to an array with MAX_SKB_FRAGS + 1 entries.  This
    means that allocating tx_rings with kzalloc() may fail because there
    is not enough contiguous memory for the new, much bigger size.  Fix
    this regression by allocating the rings with vmalloc() instead.
    
    Obtained from:	Linux commit 10313cbb92206450b450e14f2b3f6ccde42d9a34

--- sys/ofed/drivers/infiniband/ulp/ipoib/ipoib_main.c
+++ sys/ofed/drivers/infiniband/ulp/ipoib/ipoib_main.c
@@ -799,7 +799,7 @@ ipoib_dev_init(struct ipoib_dev_priv *priv, struct ib_device *ca, int port)
 		goto out;
 	}
 
-	priv->tx_ring = kzalloc(ipoib_sendq_size * sizeof *priv->tx_ring, GFP_KERNEL);
+	priv->tx_ring = vmalloc(ipoib_sendq_size * sizeof *priv->tx_ring);
 	if (!priv->tx_ring) {
 		printk(KERN_WARNING "%s: failed to allocate TX ring (%d entries)\n",
 		       ca->name, ipoib_sendq_size);
@@ -815,7 +815,7 @@ ipoib_dev_init(struct ipoib_dev_priv *priv, struct ib_device *ca, int port)
 	return 0;
 
 out_tx_ring_cleanup:
-	kfree(priv->tx_ring);
+	vfree(priv->tx_ring);
 
 out_rx_ring_cleanup:
 	kfree(priv->rx_ring);
@@ -866,7 +866,7 @@ ipoib_dev_cleanup(struct ipoib_dev_priv *priv)
 	ipoib_ib_dev_cleanup(priv);
 
 	kfree(priv->rx_ring);
-	kfree(priv->tx_ring);
+	vfree(priv->tx_ring);
 
 	priv->rx_ring = NULL;
 	priv->tx_ring = NULL;
