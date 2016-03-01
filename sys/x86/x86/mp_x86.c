223c4c24d0bd993a8720810267f325a9851ed9b8
John Baldwin <jhb@FreeBSD.org>
2016-03-01 14:45:45 -0800

    Axe #if 0'd code.

--- sys/x86/x86/mp_x86.c
+++ sys/x86/x86/mp_x86.c
@@ -933,11 +933,6 @@ init_secondary_tail(void)
 	while (atomic_load_acq_int(&smp_started) == 0)
 		ia32_pause();
 
-#if 0
-	/* Start per-CPU event timers. */
-	cpu_initclocks_ap();
-#endif
-
 	sched_throw(NULL);
 
 	panic("scheduler returned us to %s", __func__);
