375f01cef776d0727e11b937f647c191dfb56b69
John Baldwin <jhb@FreeBSD.org>
2016-04-22 17:35:34 -0700

    Debug.

--- sys/dev/cxgbe/tom/t4_ddp.c
+++ sys/dev/cxgbe/tom/t4_ddp.c
@@ -1118,7 +1118,9 @@ pscmp(struct pageset *ps, struct vmspace *vm, vm_offset_t start, int npages,
 		return (1);
 
 #ifdef VM_TIMESTAMP
-	return (ps->vm == vm && ps->vm_timestamp == ps->vm->vm_map.timestamp);
+	CTR4(KTR_CXGBE, "pscmp: vm %p vs %p timestamp %u vs %u", ps->vm, vm
+	    ps->vm_timestamp, vm->vm_map.timestamp);
+	return (ps->vm == vm && ps->vm_timestamp == vm->vm_map.timestamp);
 #elif defined(VM_MAP_COMPARE)
 	return (vm_map_compare(map, start, ptoa(npages), VM_PROT_WRITE,
 	    ps->pages, npages));
