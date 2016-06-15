c03fda54100c30c495929caaf01efddb778404fa
John Baldwin <jhb@FreeBSD.org>
2016-06-14 17:08:10 -0700

    Update copyright.

--- tools/tools/decioctl/decioctl.c
+++ tools/tools/decioctl/decioctl.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2005-2006 John H. Baldwin <jhb@FreeBSD.org>
+ * Copyright (c) 2005-2006,2016 John H. Baldwin <jhb@FreeBSD.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
