bb2e641ba2a99aff7c773b308b1391daf5f6487c
John Baldwin <jhb@FreeBSD.org>
2016-06-14 17:16:41 -0700

    Compile fixes and handle unprintable group identifiers.

--- tools/tools/decioctl/decioctl.c
+++ tools/tools/decioctl/decioctl.c
@@ -25,6 +25,7 @@
  */
 
 #include <sys/ioccom.h>
+#include <ctype.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -41,19 +42,20 @@ int
 main(int ac, char **av)
 {
 	unsigned long cmd;
+	const char *name;
 	char *cp;
-	int i;
+	int group, i;
 
 	if (ac < 2)
 		usage(av);
 	printf("  command :  dir  grp num len\tname\n");
 	for (i = 1; i < ac; i++) {
-		cmd = strtoll(av[i], &cp, 0);
+		cmd = strtoul(av[i], &cp, 0);
 		if (*cp != '\0') {
 			fprintf(stderr, "Invalid integer: %s\n", av[i]);
 			usage(av);
 		}
-		printf("0x%08x: ", cmd);
+		printf("0x%08lx: ", cmd);
 		switch (cmd & IOC_DIRMASK) {
 		case IOC_VOID:
 			printf("VOID ");
@@ -68,14 +70,19 @@ main(int ac, char **av)
 			printf("INOUT");
 			break;
 		default:
-			printf("%01x ???", (cmd & IOC_DIRMASK) >> 29);
+			printf("%01lx ???", (cmd & IOC_DIRMASK) >> 29);
 			break;
 		}
-		printf(" '%c' %3d %d", IOCGROUP(cmd), cmd & 0xff,
-		    IOCPARM_LEN(cmd));
-		cp = sysdecode_ioctlname(cmd);
-		if (cp != NULL)
-			printf("\t%s", cp);
+		printf(" ");
+		group = IOCGROUP(cmd);
+		if (isprint(group))
+			printf("'%c'", group);
+		else
+			printf("%3u", group);
+		printf(" %3lu %lu", cmd & 0xff, IOCPARM_LEN(cmd));
+		name = sysdecode_ioctlname(cmd);
+		if (name != NULL)
+			printf("\t%s", name);
 		printf("\n");
 	}
 	return (0);
