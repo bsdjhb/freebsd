30c76338422aaa80a24cfa95665355d6d7237042
John Baldwin <jhb@FreeBSD.org>
2016-07-20 16:06:27 -0700

    Use log() instead of printf().

--- sys/dev/cxgbe/t4_sge.c
+++ sys/dev/cxgbe/t4_sge.c
@@ -4802,34 +4802,35 @@ t4_handle_wrerr_rpl(struct adapter *adap, const __be64 *rpl)
 		    device_get_nameunit(adap->dev), opcode);
 		return (EINVAL);
 	}
-	device_printf(adap->dev, "FW_ERROR (%s) ",
+	log(LOG_ERR, "%s: FW_ERROR (%s) ", device_get_nameunit(adap->dev),
 	    G_FW_ERROR_CMD_FATAL(be32toh(e->op_to_type)) ? "fatal" :
 	    "non-fatal");
 	switch (G_FW_ERROR_CMD_TYPE(be32toh(e->op_to_type))) {
 	case FW_ERROR_TYPE_EXCEPTION:
-		printf("exception info:\n");
+		log(LOG_ERR, "exception info:\n");
 		for (i = 0; i < nitems(e->u.exception.info); i++)
-			printf("%s%08x", i == 0 ? "\t" : " ",
+			log(LOG_ERR, "%s%08x", i == 0 ? "\t" : " ",
 			    be32toh(e->u.exception.info[i]));
-		printf("\n");
+		log(LOG_ERR, "\n");
 		break;
 	case FW_ERROR_TYPE_HWMODULE:
-		printf("HW module regaddr %08x regval %08x\n",
+		log(LOG_ERR, "HW module regaddr %08x regval %08x\n",
 		    be32toh(e->u.hwmodule.regaddr),
 		    be32toh(e->u.hwmodule.regval));
 		break;
 	case FW_ERROR_TYPE_WR:
-		printf("WR cidx %d PF %d VF %d eqid %d hdr:\n",
+		log(LOG_ERR, "WR cidx %d PF %d VF %d eqid %d hdr:\n",
 		    be16toh(e->u.wr.cidx),
 		    G_FW_ERROR_CMD_PFN(be16toh(e->u.wr.pfn_vfn)),
 		    G_FW_ERROR_CMD_VFN(be16toh(e->u.wr.pfn_vfn)),
 		    be32toh(e->u.wr.eqid));
 		for (i = 0; i < nitems(e->u.wr.wrhdr); i++)
-			printf("%s%02x", i == 0 ? "\t" : " ", e->u.wr.wrhdr[i]);
-		printf("\n");
+			log(LOG_ERR, "%s%02x", i == 0 ? "\t" : " ",
+			    e->u.wr.wrhdr[i]);
+		log(LOG_ERR, "\n");
 		break;
 	case FW_ERROR_TYPE_ACL:
-		printf("ACL cidx %d PF %d VF %d eqid %d %s",
+		log(LOG_ERR, "ACL cidx %d PF %d VF %d eqid %d %s",
 		    be16toh(e->u.acl.cidx),
 		    G_FW_ERROR_CMD_PFN(be16toh(e->u.acl.pfn_vfn)),
 		    G_FW_ERROR_CMD_VFN(be16toh(e->u.acl.pfn_vfn)),
@@ -4837,11 +4838,11 @@ t4_handle_wrerr_rpl(struct adapter *adap, const __be64 *rpl)
 		    G_FW_ERROR_CMD_MV(be16toh(e->u.acl.mv_pkd)) ? "vlanid" :
 		    "MAC");
 		for (i = 0; i < nitems(e->u.acl.val); i++)
-			printf(" %02x", e->u.acl.val[i]);
-		printf("\n");
+			log(LOG_ERR, " %02x", e->u.acl.val[i]);
+		log(LOG_ERR, "\n");
 		break;
 	default:
-		printf("type %#x\n",
+		log(LOG_ERR, "type %#x\n",
 		    G_FW_ERROR_CMD_TYPE(be32toh(e->op_to_type)));
 		return (EINVAL);
 	}
