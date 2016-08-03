ffc4488536bf81b82676fc6eed7920167b960050
John Baldwin <jhb@FreeBSD.org>
2016-08-03 09:01:01 -0700

    Add __printflike().

--- sys/dev/pci/pci_iov.h
+++ sys/dev/pci/pci_iov.h
@@ -34,7 +34,7 @@
 struct nvlist;
 
 int	pci_iov_attach_name(device_t dev, struct nvlist *pf_schema,
-	    struct nvlist *vf_schema, const char *fmt, ...);
+	    struct nvlist *vf_schema, const char *fmt, ...) __printflike(4, 5);
 
 static __inline int
 pci_iov_attach(device_t dev, struct nvlist *pf_schema, struct nvlist *vf_schema)
