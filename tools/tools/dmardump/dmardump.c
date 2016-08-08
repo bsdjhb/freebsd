9d4dca93c9a66bfc493b346e89193f6bf77d6f3c
John Baldwin <jhb@FreeBSD.org>
2016-08-08 11:20:57 -0700

    Add newline after each line.

--- tools/tools/dmardump/dmardump.c
+++ tools/tools/dmardump/dmardump.c
@@ -139,6 +139,7 @@ dump_context_table(int segment, int bus, uint64_t base_addr)
 		printf(" SLPT %#jx", (uintmax_t)(ctx[idx].ctx1 &
 		    DMAR_CTX1_ASR_MASK));
 		printf(" domain %d", (int)((ctx[idx].ctx2 >> 8) & 0xffff));
+		printf("\n");
 	}
 }
 
