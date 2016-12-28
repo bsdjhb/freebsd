1a474b04d1da76f2d325265bac28019a9b14c5bc
John Baldwin <jhb@FreeBSD.org>
2016-12-28 15:41:08 -0800

    Add basic session management.
    No crypto algorithms are registered yet.

--- sys/dev/cxgbe/t4_crypto.c
+++ sys/dev/cxgbe/t4_crypto.c
@@ -30,13 +30,32 @@ __FBSDID("$FreeBSD$");
 
 #include <sys/types.h>
 #include <sys/bus.h>
+#include <sys/lock.h>
+#include <sys/malloc.h>
+#include <sys/mutex.h>
 #include <sys/module.h>
 
+#include <opencrypto/cryptodev.h>
+
+#include "cryptodev_if.h"
+
 #include "common/common.h"
+
+static MALLOC_DEFINE(M_CCR, "ccr", "Chelsio T6 crypto");
+
+struct ccr_session {
+	bool active;
+	int pending;
+};
 
 struct ccr_softc {
 	struct adapter *adapter;
 	device_t dev;
+	uint32_t cid;
+	struct ccr_session *sessions;
+	int nsessions;
+	struct mtx lock;
+	bool detaching;
 };
 
 static void
@@ -62,18 +81,139 @@ static int
 ccr_attach(device_t dev)
 {
 	struct ccr_softc *sc;
+	int32_t cid;
 
 	sc = device_get_softc(dev);
 	sc->adapter = device_get_softc(device_get_parent(dev));
+	if (sc->adapter->sge.nofldrxq == 0) {
+		device_printf(dev,
+		    "parent device does not have offload queues\n");
+		return (ENXIO);
+	}
 	sc->dev = dev;
+	cid = crypto_get_driverid(dev, CRYPTOCAP_F_HARDWARE);
+	if (cid < 0) {
+		device_printf(dev, "could not get crypto driver id\n");
+		return (ENXIO);
+	}
+	sc->cid = cid;
+
+	mtx_init(&sc->lock, "ccr", NULL, MTX_DEF);
+
+#ifdef notyet
+	crypto_register(cid, CRYPTO_SHA1, 0, 0);
+	crypto_register(cid, CRYPTO_SHA1_HMAC, 0, 0);
+	crypto_register(cid, CRYPTO_SHA2_256_HMAC, 0, 0);
+	crypto_register(cid, CRYPTO_SHA2_384_HMAC, 0, 0);
+	crypto_register(cid, CRYPTO_SHA2_512_HMAC, 0, 0);
+	crypto_register(cid, CRYPTO_AES_CBC, 0, 0);
+	crypto_register(cid, CRYPTO_AES_ICM, 0, 0);
+	crypto_register(cid, CRYPTO_AES_NIST_GMAC, 0, 0);
+	crypto_register(cid, CRYPTO_AES_NIST_GCM_16, 0, 0);
+	crypto_register(cid, CRYPTO_AES_XTS, 0, 0);
+#endif
 	return (0);
 }
 
 static int
 ccr_detach(device_t dev)
 {
+	struct ccr_softc *sc;
+	int i;
 
+	sc = device_get_softc(dev);
+
+	mtx_lock(&sc->lock);
+	for (i = 0; i < sc->nsessions; i++) {
+		if (sc->sessions[i].pending != 0) {
+			mtx_unlock(&sc->lock);
+			return (EBUSY);
+		}
+	}
+	sc->detaching = true;
+	mtx_unlock(&sc->lock);
+
+	crypto_unregister_all(sc->cid);
+	free(sc->sessions, M_CCR);
+	mtx_destroy(&sc->lock);
 	return (0);
+}
+
+static int
+ccr_newsession(device_t dev, uint32_t *sidp, struct cryptoini *cri)
+{
+	struct ccr_softc *sc;
+	struct ccr_session *s;
+	int i, sess;
+
+	if (sidp == NULL || cri == NULL)
+		return (EINVAL);
+
+	sc = device_get_softc(dev);
+	mtx_lock(&sc->lock);
+	if (sc->detaching) {
+		mtx_unlock(&sc->lock);
+		return (ENXIO);
+	}
+	sess = -1;
+	for (i = 0; i < sc->nsessions; i++) {
+		if (!sc->sessions[i].active && sc->sessions[i].pending == 0) {
+			sess = i;
+			break;
+		}
+	}
+	if (sess == -1) {
+		s = malloc(sizeof(*s) * (sc->nsessions + 1), M_CCR,
+		    M_NOWAIT | M_ZERO);
+		if (s == NULL) {
+			mtx_unlock(&sc->lock);
+			return (ENOMEM);
+		}
+		if (sc->sessions != NULL)
+			memcpy(s, sc->sessions, sizeof(*s) * sc->nsessions);
+		sess = sc->nsessions;
+		free(sc->sessions, M_CCR);
+		sc->sessions = s;
+		sc->nsessions++;
+	}
+
+	s = &sc->sessions[sess];
+	s->active = true;
+	mtx_unlock(&sc->lock);
+
+	*sidp = sess;
+	return (0);
+}
+
+static int
+ccr_freesession(device_t dev, uint64_t tid)
+{
+	struct ccr_softc *sc;
+	uint32_t sid;
+	int error;
+
+	sc = device_get_softc(dev);
+	sid = CRYPTO_SESID2LID(tid);
+	mtx_lock(&sc->lock);
+	if (sid >= sc->nsessions || !sc->sessions[sid].active)
+		error = EINVAL;
+	else {
+		if (sc->sessions[sid].pending != 0)
+			device_printf(dev,
+			    "session %d freed with %d pending requests\n", sid,
+			    sc->sessions[sid].pending);
+		sc->sessions[sid].active = false;
+		error = 0;
+	}
+	mtx_unlock(&sc->lock);
+	return (error);
+}
+
+static int
+ccr_process(device_t dev, struct cryptop *crp, int hint)
+{
+
+	return (ENXIO);
 }
 
 static device_method_t ccr_methods[] = {
@@ -81,6 +221,10 @@ static device_method_t ccr_methods[] = {
 	DEVMETHOD(device_probe,		ccr_probe),
 	DEVMETHOD(device_attach,	ccr_attach),
 	DEVMETHOD(device_detach,	ccr_detach),
+
+	DEVMETHOD(cryptodev_newsession,	ccr_newsession),
+	DEVMETHOD(cryptodev_freesession, ccr_freesession),
+	DEVMETHOD(cryptodev_process,	ccr_process),
 
 	DEVMETHOD_END
 };
