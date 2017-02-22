f501167b88645344a6a27f8d4c932926ddd52eef
John Baldwin <jhb@FreeBSD.org>
2017-02-21 17:03:04 -0800

    First cut at block cipher tests.

--- tools/tools/crypto/cryptocheck.c
+++ tools/tools/crypto/cryptocheck.c
@@ -80,10 +80,9 @@
 struct alg {
 	const char *name;
 	int alg;
-	enum { T_NONE, T_HMAC } type;
-	union {
-		const EVP_MD *(*evp_md)(void);
-	};
+	enum { T_NONE, T_HMAC, T_BLKCIPHER } type;
+	const EVP_CIPHER *(*evp_cipher)(void);
+	const EVP_MD *(*evp_md)(void);
 		
 } algs[] = {
 	{ .name = "all", .alg = 0, .type = T_NONE },
@@ -96,6 +95,23 @@ struct alg {
 	  .evp_md = EVP_sha384 },
 	{ .name = "sha512", .alg = CRYPTO_SHA2_512_HMAC, .type = T_HMAC,
 	  .evp_md = EVP_sha512 },
+	{ .name = "blkcipher", .alg = 0, .type = T_BLKCIPHER },
+	{ .name = "aes", .alg = CRYPTO_AES_CBC, .type = T_BLKCIPHER,
+	  .evp_cipher = EVP_aes_128_cbc },
+	{ .name = "aes192", .alg = CRYPTO_AES_CBC, .type = T_BLKCIPHER,
+	  .evp_cipher = EVP_aes_192_cbc },
+	{ .name = "aes256", .alg = CRYPTO_AES_CBC, .type = T_BLKCIPHER,
+	  .evp_cipher = EVP_aes_256_cbc },
+	{ .name = "aes-ctr", .alg = CRYPTO_AES_ICM, .type = T_BLKCIPHER,
+	  .evp_cipher = EVP_aes_128_ctr },
+	{ .name = "aes-ctr192", .alg = CRYPTO_AES_ICM, .type = T_BLKCIPHER,
+	  .evp_cipher = EVP_aes_192_ctr },
+	{ .name = "aes-ctr256", .alg = CRYPTO_AES_ICM, .type = T_BLKCIPHER,
+	  .evp_cipher = EVP_aes_256_ctr },
+	{ .name = "aes-xts", .alg = CRYPTO_AES_XTS, .type = T_BLKCIPHER,
+	  .evp_cipher = EVP_aes_128_xts },
+	{ .name = "aes-xts256", .alg = CRYPTO_AES_XTS, .type = T_BLKCIPHER,
+	  .evp_cipher = EVP_aes_256_xts },
 };
 
 static bool verbose;
@@ -200,7 +216,7 @@ alloc_buffer(size_t len)
 }
 
 static bool
-ocf_hmac(struct alg *alg, char *buffer, size_t size, char *key,
+ocf_hmac(struct alg *alg, const char *buffer, size_t size, const char *key,
     size_t key_len, char *digest, int *cridp)
 {
 	struct session2_op sop;
@@ -211,7 +227,7 @@ ocf_hmac(struct alg *alg, char *buffer, size_t size, char *key,
 	memset(&cop, 0, sizeof(cop));
 	sop.crid = crid;
 	sop.mackeylen = key_len;
-	sop.mackey = key;
+	sop.mackey = (char *)key;
 	sop.mac = alg->alg;
 	fd = crget();
 	if (ioctl(fd, CIOCGSESSION2, &sop) < 0) {
@@ -224,7 +240,7 @@ ocf_hmac(struct alg *alg, char *buffer, size_t size, char *key,
 	cop.ses = sop.ses;
 	cop.op = 0;
 	cop.len = size;
-	cop.src = buffer;
+	cop.src = (char *)buffer;
 	cop.dst = NULL;
 	cop.mac = digest;
 	cop.iv = NULL;
@@ -291,12 +307,161 @@ run_hmac_test(struct alg *alg, size_t size)
 }
 
 static void
+openssl_cipher(struct alg *alg, const EVP_CIPHER *cipher, const char *key,
+    const char *iv, const char *input, char *output, size_t size, int enc)
+{
+	EVP_CIPHER_CTX *ctx;
+	int outl, total;
+
+	ctx = EVP_CIPHER_CTX_new();
+	if (ctx == NULL)
+		errx(1, "OpenSSL %s (%zu) ctx new failed: %s", alg->name,
+		    size, ERR_error_string(ERR_get_error(), NULL));
+	if (EVP_CipherInit_ex(ctx, cipher, NULL, (const u_char *)key,
+	    (const u_char *)iv, enc) != 1)
+		errx(1, "OpenSSL %s (%zu) ctx init failed: %s", alg->name,
+		    size, ERR_error_string(ERR_get_error(), NULL));
+	if (EVP_CipherUpdate(ctx, (u_char *)output, &outl,
+	    (const u_char *)input, size) != 1)
+		errx(1, "OpenSSL %s (%zu) cipher update failed: %s", alg->name,
+		    size, ERR_error_string(ERR_get_error(), NULL));
+	total = outl;
+	if (EVP_CipherFinal_ex(ctx, (u_char *)output + outl, &outl) != 1)
+		errx(1, "OpenSSL %s (%zu) cipher final failed: %s", alg->name,
+		    size, ERR_error_string(ERR_get_error(), NULL));
+	total += outl;
+	if (total != size)
+		errx(1, "OpenSSL %s (%zu) cipher size mismatch: %d", alg->name,
+		    size, total);
+	EVP_CIPHER_CTX_free(ctx);
+}
+
+static bool
+ocf_cipher(struct alg *alg, const char *key, size_t key_len,
+    const char *iv, const char *input, char *output, size_t size, int enc,
+    int *cridp)
+{
+	struct session2_op sop;
+	struct crypt_op cop;
+	int fd;
+
+	memset(&cop, 0, sizeof(cop));
+	sop.crid = crid;
+	sop.keylen = key_len;
+	sop.key = (char *)key;
+	sop.cipher = alg->alg;
+	fd = crget();
+	if (ioctl(fd, CIOCGSESSION2, &sop) < 0) {
+		warn("cryptodev %s block cipher not supported for device %s",
+		    alg->name, crfind(crid));
+		close(fd);
+		return (false);
+	}
+
+	cop.ses = sop.ses;
+	cop.op = enc ? COP_ENCRYPT : COP_DECRYPT;
+	cop.len = size;
+	cop.src = (char *)input;
+	cop.dst = output;
+	cop.mac = NULL;
+	cop.iv = (char *)iv;
+
+	if (ioctl(fd, CIOCCRYPT, &cop) < 0)
+		err(1, "ioctl(CIOCCRYPT)");
+
+	if (ioctl(fd, CIOCFSESSION, &sop.ses) < 0)
+		warn("ioctl(CIOCFSESSION)");
+
+	close(fd);
+	*cridp = sop.crid;
+	return (true);
+}
+
+static void
+run_blkcipher_test(struct alg *alg, size_t size)
+{
+	const EVP_CIPHER *cipher;
+	char *buffer, *cleartext, *ciphertext;
+	char *iv, *key;
+	u_int iv_len, key_len;
+	int crid;
+
+	cipher = alg->evp_cipher();
+	if (size % EVP_CIPHER_block_size(cipher) != 0) {
+		if (verbose)
+			printf(
+			    "%s (%zu): invalid buffer size (block size %d)\n",
+			    alg->name, size, EVP_CIPHER_block_size(cipher));
+		return;
+	}
+			    
+	key_len = EVP_CIPHER_key_length(cipher);
+	iv_len = EVP_CIPHER_iv_length(cipher);
+
+	key = alloc_buffer(key_len);
+	cleartext = alloc_buffer(size);
+	buffer = malloc(size);
+	ciphertext = malloc(size);
+
+	/* OpenSSL cipher. */
+	openssl_cipher(alg, cipher, key, iv, cleartext, ciphertext, size, 1);
+	if (memcmp(cleartext, ciphertext, size) == 0)
+		errx(1, "OpenSSL %s (%zu): cipher text unchanged", alg->name,
+		    size);
+	openssl_cipher(alg, cipher, key, iv, ciphertext, buffer, size, 0);
+	if (memcmp(cleartext, buffer, size) != 0) {
+		printf("OpenSSL %s (%zu): cipher mismatch:", alg->name, size);
+		printf("original:\n");
+		hexdump(cleartext, size, NULL, 0);
+		printf("decrypted:\n");
+		hexdump(buffer, size, NULL, 0);
+		exit(1);
+	}
+
+	/* OCF encrypt. */
+	if (!ocf_cipher(alg, key, key_len, iv, cleartext, buffer, size, 1,
+	    &crid))
+		goto out;
+	if (memcmp(ciphertext, buffer, size) != 0) {
+		printf("%s (%zu) encryption mismatch:\n", alg->name, size);
+		printf("control:\n");
+		hexdump(ciphertext, size, NULL, 0);
+		printf("test (cryptodev device %s):\n", crfind(crid));
+		hexdump(buffer, size, NULL, 0);
+		goto out;
+	}
+	
+	/* OCF decrypt. */
+	if (!ocf_cipher(alg, key, key_len, iv, ciphertext, buffer, size, 0,
+	    &crid))
+		goto out;
+	if (memcmp(cleartext, buffer, size) != 0) {
+		printf("%s (%zu) decryption mismatch:\n", alg->name, size);
+		printf("control:\n");
+		hexdump(cleartext, size, NULL, 0);
+		printf("test (cryptodev device %s):\n", crfind(crid));
+		hexdump(buffer, size, NULL, 0);
+	} else if (verbose)
+		printf("%s (%zu) matched (cryptodev device %s)\n",
+		    alg->name, size, crfind(crid));
+	
+out:
+	free(ciphertext);
+	free(buffer);
+	free(cleartext);
+	free(key);
+}
+
+static void
 run_test(struct alg *alg, size_t size)
 {
 
 	switch (alg->type) {
 	case T_HMAC:
 		run_hmac_test(alg, size);
+		break;
+	case T_BLKCIPHER:
+		run_blkcipher_test(alg, size);
 		break;
 	case T_NONE:
 		assert(false);
