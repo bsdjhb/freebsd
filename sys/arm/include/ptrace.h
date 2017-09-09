dc0640650b75673092ca72537c0a7b61b3ea9c5b
John Baldwin <jhb@FreeBSD.org>
2017-09-09 06:39:57 -0700

    Add ptrace ops to fetch and store VFP registers.

--- sys/arm/include/ptrace.h
+++ sys/arm/include/ptrace.h
@@ -4,5 +4,16 @@
 #ifndef _MACHINE_PTRACE_H_
 #define _MACHINE_PTRACE_H_
 
+#define	__HAVE_PTRACE_MACHDEP
+
+/* Must match mcontext_vfp_t. */
+struct vfpreg {
+	__uint64_t	vfp_reg[32];
+	__uint32_t	vfp_scr;
+};
+
+#define	PT_GETVFPREGS	(PT_FIRSTMACH + 0)
+#define	PT_SETVFPREGS	(PT_FIRSTMACH + 1)
+
 #endif /* !_MACHINE_PTRACE_H */
 
