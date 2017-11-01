50e031559d58ccca62183f1eaea25f6bab58cdae
John Baldwin <jhb@FreeBSD.org>
2017-11-01 15:47:24 -0700

    Compile.

--- sys/dev/cxgbe/tom/t4_tom.c
+++ sys/dev/cxgbe/tom/t4_tom.c
@@ -662,7 +662,7 @@ void
 set_ulp_mode(struct toepcb *toep, int ulp_mode)
 {
 
-	CTR5(KTR_CXGBE, "%s: toep %p (tid %d) ulp_mode %d",
+	CTR4(KTR_CXGBE, "%s: toep %p (tid %d) ulp_mode %d",
 	    __func__, toep, toep->tid, ulp_mode);
 	toep->ulp_mode = ulp_mode;
 	switch (ulp_mode) {
