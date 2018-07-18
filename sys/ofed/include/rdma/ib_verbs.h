751c9ef7d15c72c63e30e7e03d717d669cadc78f
Bart Van Assche <bart.vanassche@wdc.com>
2018-07-18 09:25:16 -0700

    IB/core: Allow ULPs to specify NULL as the third ib_post_(send|recv|srq_recv)() argument
    This patch does not change the behavior of the modified functions.
    
    Obtained from:	Linux commit bb039a870c0593a4deaa72c2693d02a87723305c

--- sys/ofed/include/rdma/ib_verbs.h
+++ sys/ofed/include/rdma/ib_verbs.h
@@ -3204,7 +3204,9 @@ static inline int ib_post_srq_recv(struct ib_srq *srq,
 				   const struct ib_recv_wr *recv_wr,
 				   const struct ib_recv_wr **bad_recv_wr)
 {
-	return srq->device->post_srq_recv(srq, recv_wr, bad_recv_wr);
+	const struct ib_recv_wr *dummy;
+
+	return srq->device->post_srq_recv(srq, recv_wr, bad_recv_wr ? : &dummy);
 }
 
 /**
@@ -3317,7 +3319,9 @@ static inline int ib_post_send(struct ib_qp *qp,
 			       const struct ib_send_wr *send_wr,
 			       const struct ib_send_wr **bad_send_wr)
 {
-	return qp->device->post_send(qp, send_wr, bad_send_wr);
+	const struct ib_send_wr *dummy;
+
+	return qp->device->post_send(qp, send_wr, bad_send_wr ? : &dummy);
 }
 
 /**
@@ -3332,7 +3336,9 @@ static inline int ib_post_recv(struct ib_qp *qp,
 			       const struct ib_recv_wr *recv_wr,
 			       const struct ib_recv_wr **bad_recv_wr)
 {
-	return qp->device->post_recv(qp, recv_wr, bad_recv_wr);
+	const struct ib_recv_wr *dummy;
+
+	return qp->device->post_recv(qp, recv_wr, bad_recv_wr ? : &dummy);
 }
 
 struct ib_cq *__ib_alloc_cq_user(struct ib_device *dev, void *private,
