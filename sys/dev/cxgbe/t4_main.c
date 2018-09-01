d0de6ebfa4a2849dcadc01cf5c5cb1458f02e62c
John Baldwin <jhb@FreeBSD.org>
2018-08-31 17:27:40 -0700

    Move CLIP table handling out of TOM and into the base driver.
    - Store the clip table in 'struct adapter' instead of in the TOM softc.
    - Init the clip table during attach and teardown during detach.
    
    This does mean that we update the clip table even if TOE is not enabled,
    but non-TOE things need the CLIP table anyway.

--- sys/dev/cxgbe/t4_main.c
+++ sys/dev/cxgbe/t4_main.c
@@ -84,6 +84,7 @@ __FBSDID("$FreeBSD$");
 #include "common/t4_regs.h"
 #include "common/t4_regs_values.h"
 #include "cudbg/cudbg.h"
+#include "t4_clip.h"
 #include "t4_ioctl.h"
 #include "t4_l2t.h"
 #include "t4_mp_ring.h"
@@ -1148,6 +1149,7 @@ t4_attach(device_t dev)
 #ifdef RATELIMIT
 	t4_init_etid_table(sc);
 #endif
+	t4_init_clip_table(sc);
 	if (sc->vres.key.size != 0)
 		sc->key_map = vmem_create("T4TLS key map", sc->vres.key.start,
 		    sc->vres.key.size, 32, 0, M_FIRSTFIT | M_WAITOK);
@@ -1438,6 +1440,7 @@ t4_detach_common(device_t dev)
 #endif
 	if (sc->key_map)
 		vmem_destroy(sc->key_map);
+	t4_destroy_clip_table(sc);
 
 #if defined(TCP_OFFLOAD) || defined(RATELIMIT)
 	free(sc->sge.ofld_txq, M_CXGBE);
@@ -10454,6 +10457,7 @@ mod_event(module_t mod, int cmd, void *arg)
 			sx_init(&t4_uld_list_lock, "T4/T5 ULDs");
 			SLIST_INIT(&t4_uld_list);
 #endif
+			t4_clip_modload();
 			t4_tracer_modload();
 			tweak_tunables();
 		}
@@ -10493,6 +10497,7 @@ mod_event(module_t mod, int cmd, void *arg)
 
 			if (t4_sge_extfree_refs() == 0) {
 				t4_tracer_modunload();
+				t4_clip_modunload();
 #ifdef TCP_OFFLOAD
 				sx_destroy(&t4_uld_list_lock);
 #endif
