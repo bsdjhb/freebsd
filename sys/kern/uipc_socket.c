955bd897aa671997c8e39ed716980546d26eb5e6
John Baldwin <jhb@FreeBSD.org>
2019-11-21 11:18:00 -0800

    Add a SO_WANT_KTLS socket option.
    This is a hint that KTLS might be used.  The socket might not end up
    using TLS at all, but if it does, the application will use KTLS.

--- sys/kern/uipc_socket.c
+++ sys/kern/uipc_socket.c
@@ -3013,6 +3013,7 @@ sosetopt(struct socket *so, struct sockopt *sopt)
 		case SO_NOSIGPIPE:
 		case SO_NO_DDP:
 		case SO_NO_OFFLOAD:
+		case SO_WANT_KTLS:
 			error = sooptcopyin(sopt, &optval, sizeof optval,
 			    sizeof optval);
 			if (error)
@@ -3234,6 +3235,7 @@ sogetopt(struct socket *so, struct sockopt *sopt)
 		case SO_NOSIGPIPE:
 		case SO_NO_DDP:
 		case SO_NO_OFFLOAD:
+		case SO_WANT_KTLS:
 			optval = so->so_options & sopt->sopt_name;
 integer:
 			error = sooptcopyout(sopt, &optval, sizeof optval);
