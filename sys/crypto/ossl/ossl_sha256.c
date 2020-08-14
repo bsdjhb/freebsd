fbbeebd0d9eafb79ee6379eacf2fb9794418a89b
John Baldwin <jhb@FreeBSD.org>
2020-08-14 14:18:20 -0700

    Fix licenses to match their source files.
    Add a few missing $FreeBSD$.

--- sys/crypto/ossl/ossl_sha256.c
+++ sys/crypto/ossl/ossl_sha256.c
@@ -1,11 +1,14 @@
 /*
- * Copyright 2011-2016 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright 2004-2016 The OpenSSL Project Authors. All Rights Reserved.
  *
  * Licensed under the OpenSSL license (the "License").  You may not use
  * this file except in compliance with the License.  You can obtain a copy
  * in the file LICENSE in the source distribution or at
  * https://www.openssl.org/source/license.html
  */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
 #include <sys/libkern.h>
 #include <sys/malloc.h>
