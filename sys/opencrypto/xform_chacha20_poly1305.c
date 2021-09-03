cce957eaa6765f2abb508967927531ee69234438
John Baldwin <jhb@FreeBSD.org>
2021-09-03 15:20:30 -0700

    crypto: Permit variable-sized IVs for ciphers with a reinit hook.
    Add a 'len' argument to the reinit hook in 'struct enc_xform' to
    permit support for AEAD ciphers such as AES-CCM and Chacha20-Poly1305
    which support different nonce lengths.
    
    Sponsored by:	Chelsio Communications, FreeBSD Foundation

--- sys/opencrypto/xform_chacha20_poly1305.c
+++ sys/opencrypto/xform_chacha20_poly1305.c
@@ -50,9 +50,12 @@ chacha20_poly1305_setkey(void *vctx, const uint8_t *key, int len)
 }
 
 static void
-chacha20_poly1305_reinit(void *vctx, const uint8_t *iv)
+chacha20_poly1305_reinit(void *vctx, const uint8_t *iv, size_t len)
 {
 	struct chacha20_poly1305_cipher_ctx *ctx = vctx;
+
+	KASSERT(len == sizeof(ctx->nonce),
+	    ("%s: invalid nonce length", __func__));
 
 	/* Block 0 is used for the poly1305 key. */
 	memcpy(ctx->nonce, iv, sizeof(ctx->nonce));
