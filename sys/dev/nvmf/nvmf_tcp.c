63b7ede291b4000afa27e4d4d77da9712b0742e7
John Baldwin <jhb@FreeBSD.org>
2024-01-18 11:30:59 -0800

    nvmf_tcp: Factor out shared code into nvmf_tcp.h
    In particular, a routine to validate common PDU header fields.

--- sys/dev/nvmf/nvmf_tcp.c
+++ sys/dev/nvmf/nvmf_tcp.c
@@ -48,6 +48,8 @@
 #include <netinet/in.h>
 #include <dev/nvme/nvme.h>
 #include <dev/nvmf/nvmf.h>
+#include <dev/nvmf/nvmf_proto.h>
+#include <dev/nvmf/nvmf_tcp.h>
 #include <dev/nvmf/nvmf_transport.h>
 #include <dev/nvmf/nvmf_transport_internal.h>
 
@@ -147,13 +149,6 @@ struct nvmf_tcp_capsule {
 
 	STAILQ_ENTRY(nvmf_tcp_capsule) link;
 };
-
-#define	NVME_SGL_TYPE_ICD						\
-	NVME_SGL_TYPE(NVME_SGL_TYPE_DATA_BLOCK, NVME_SGL_SUBTYPE_OFFSET)
-
-#define	NVME_SGL_TYPE_COMMAND_BUFFER					\
-	NVME_SGL_TYPE(NVME_SGL_TYPE_TRANSPORT_DATA_BLOCK,		\
-	    NVME_SGL_SUBTYPE_TRANSPORT)
 
 #define	TCAP(nc)	((struct nvmf_tcp_capsule *)(nc))
 #define	TQP(qp)		((struct nvmf_tcp_qpair *)(qp))
@@ -332,10 +327,11 @@ nvmf_tcp_validate_pdu(struct nvmf_tcp_qpair *qp, struct nvmf_tcp_rxpdu *pdu)
 {
 	const struct nvme_tcp_common_pdu_hdr *ch;
 	struct mbuf *m = pdu->m;
-	uint32_t data_len, plen;
+	uint32_t data_len, fei, plen;
 	uint32_t digest, rx_digest;
-	u_int full_hlen, hlen, expected_hlen;
-	uint8_t digest_flags, valid_flags;
+	u_int hlen;
+	int error;
+	uint16_t fes;
 
 	/* Determine how large of a PDU header to return for errors. */
 	ch = pdu->hdr;
@@ -344,232 +340,13 @@ nvmf_tcp_validate_pdu(struct nvmf_tcp_qpair *qp, struct nvmf_tcp_rxpdu *pdu)
 	if (hlen < sizeof(*ch) || hlen > plen)
 		hlen = sizeof(*ch);
 
-	/*
-	 * Errors must be reported for the lowest incorrect field
-	 * first, so validate fields in order.
-	 */
-
-	/* Validate pdu_type. */
-
-	/* Controllers only receive PDUs with a PDU direction of 0. */
-	if (qp->qp.nq_controller != (ch->pdu_type & 0x01) == 0) {
-		printf("NVMe/TCP: Invalid PDU type %u\n", ch->pdu_type);
-		nvmf_tcp_report_error(qp,
-		    NVME_TCP_TERM_REQ_FES_INVALID_HEADER_FIELD, 0, m, hlen);
-		return (EBADMSG);
-	}
-
-	switch (ch->pdu_type) {
-	case NVME_TCP_PDU_TYPE_IC_REQ:
-	case NVME_TCP_PDU_TYPE_IC_RESP:
-		/* Shouldn't get these in the kernel. */
-		printf("NVMe/TCP: Received Initialize Connection PDU\n");
-		nvmf_tcp_report_error(qp,
-		    NVME_TCP_TERM_REQ_FES_INVALID_HEADER_FIELD, 0, m, hlen);
-		return (EBADMSG);
-	case NVME_TCP_PDU_TYPE_H2C_TERM_REQ:
-	case NVME_TCP_PDU_TYPE_C2H_TERM_REQ:
-		/*
-		 * 7.4.7 Termination requests with invalid PDU lengths
-		 * result in an immediate connection termination
-		 * without reporting an error.
-		 */
-		if (plen < sizeof(struct nvme_tcp_term_req_hdr) ||
-		    plen > NVME_TCP_TERM_REQ_PDU_MAX_SIZE) {
-			printf("NVMe/TCP: Received invalid termination request\n");
-			return (ECONNRESET);
-		}
-		break;
-	case NVME_TCP_PDU_TYPE_CAPSULE_CMD:
-	case NVME_TCP_PDU_TYPE_CAPSULE_RESP:
-	case NVME_TCP_PDU_TYPE_H2C_DATA:
-	case NVME_TCP_PDU_TYPE_C2H_DATA:
-	case NVME_TCP_PDU_TYPE_R2T:
-		break;
-	default:
-		printf("NVMe/TCP: Invalid PDU type %u\n", ch->pdu_type);
-		nvmf_tcp_report_error(qp,
-		    NVME_TCP_TERM_REQ_FES_INVALID_HEADER_FIELD, 0, m, hlen);
-		return (EBADMSG);
-	}
-
-	/* Validate flags. */
-	switch (ch->pdu_type) {
-	default:
-		__assert_unreachable();
-		break;
-	case NVME_TCP_PDU_TYPE_H2C_TERM_REQ:
-	case NVME_TCP_PDU_TYPE_C2H_TERM_REQ:
-		valid_flags = 0;
-		break;
-	case NVME_TCP_PDU_TYPE_CAPSULE_CMD:
-		valid_flags = NVME_TCP_CH_FLAGS_HDGSTF |
-		    NVME_TCP_CH_FLAGS_DDGSTF;
-		break;
-	case NVME_TCP_PDU_TYPE_CAPSULE_RESP:
-	case NVME_TCP_PDU_TYPE_R2T:
-		valid_flags = NVME_TCP_CH_FLAGS_HDGSTF;
-		break;
-	case NVME_TCP_PDU_TYPE_H2C_DATA:
-		valid_flags = NVME_TCP_CH_FLAGS_HDGSTF |
-		    NVME_TCP_CH_FLAGS_DDGSTF | NVME_TCP_H2C_DATA_FLAGS_LAST_PDU;
-		break;
-	case NVME_TCP_PDU_TYPE_C2H_DATA:
-		valid_flags = NVME_TCP_CH_FLAGS_HDGSTF |
-		    NVME_TCP_CH_FLAGS_DDGSTF | NVME_TCP_C2H_DATA_FLAGS_LAST_PDU |
-		    NVME_TCP_C2H_DATA_FLAGS_SUCCESS;
-		break;
-	}
-	if ((ch->flags & ~valid_flags) != 0) {
-		printf("NVMe/TCP: Invalid PDU header flags %#x\n", ch->flags);
-		nvmf_tcp_report_error(qp,
-		    NVME_TCP_TERM_REQ_FES_INVALID_HEADER_FIELD, 1, m, hlen);
-		return (EBADMSG);
-	}
-
-	/* Verify that digests are present iff enabled. */
-	digest_flags = 0;
-	if (qp->header_digests)
-		digest_flags |= NVME_TCP_CH_FLAGS_HDGSTF;
-	if (qp->data_digests)
-		digest_flags |= NVME_TCP_CH_FLAGS_DDGSTF;
-	if ((digest_flags & valid_flags) !=
-	    (ch->flags & (NVME_TCP_CH_FLAGS_HDGSTF |
-	    NVME_TCP_CH_FLAGS_DDGSTF))) {
-		printf("NVMe/TCP: Invalid PDU header flags %#x\n", ch->flags);
-		nvmf_tcp_report_error(qp,
-		    NVME_TCP_TERM_REQ_FES_INVALID_HEADER_FIELD, 1, m, hlen);
-		return (EBADMSG);
-	}
-
-	/* 7.4.5.2: SUCCESS in C2H requires LAST_PDU */
-	if (ch->pdu_type == NVME_TCP_PDU_TYPE_C2H_DATA &&
-	    (ch->flags & (NVME_TCP_C2H_DATA_FLAGS_LAST_PDU |
-	    NVME_TCP_C2H_DATA_FLAGS_SUCCESS)) ==
-	    NVME_TCP_C2H_DATA_FLAGS_SUCCESS) {
-		printf("NVMe/TCP: Invalid PDU header flags %#x\n", ch->flags);
-		nvmf_tcp_report_error(qp,
-		    NVME_TCP_TERM_REQ_FES_INVALID_HEADER_FIELD, 1, m, hlen);
-		return (EBADMSG);
-	}
-
-	/* Validate hlen. */
-	switch (ch->pdu_type) {
-	default:
-		__assert_unreachable();
-		break;
-	case NVME_TCP_PDU_TYPE_H2C_TERM_REQ:
-	case NVME_TCP_PDU_TYPE_C2H_TERM_REQ:
-		expected_hlen = sizeof(struct nvme_tcp_term_req_hdr);
-		break;
-	case NVME_TCP_PDU_TYPE_CAPSULE_CMD:
-		expected_hlen = sizeof(struct nvme_tcp_cmd);
-		break;
-	case NVME_TCP_PDU_TYPE_CAPSULE_RESP:
-		expected_hlen = sizeof(struct nvme_tcp_rsp);
-		break;
-	case NVME_TCP_PDU_TYPE_H2C_DATA:
-		expected_hlen = sizeof(struct nvme_tcp_h2c_data_hdr);
-		break;
-	case NVME_TCP_PDU_TYPE_C2H_DATA:
-		expected_hlen = sizeof(struct nvme_tcp_c2h_data_hdr);
-		break;
-	case NVME_TCP_PDU_TYPE_R2T:
-		expected_hlen = sizeof(struct nvme_tcp_r2t_hdr);
-		break;
-	}
-	if (ch->hlen != expected_hlen) {
-		printf("NVMe/TCP: Invalid PDU header length %u\n", ch->hlen);
-		nvmf_tcp_report_error(qp,
-		    NVME_TCP_TERM_REQ_FES_INVALID_HEADER_FIELD, 2, m, hlen);
-		return (EBADMSG);
-	}
-
-	/* Validate pdo. */
-	full_hlen = ch->hlen;
-	if ((ch->flags & NVME_TCP_CH_FLAGS_HDGSTF) != 0)
-		full_hlen += sizeof(rx_digest);
-	switch (ch->pdu_type) {
-	default:
-		__assert_unreachable();
-		break;
-	case NVME_TCP_PDU_TYPE_H2C_TERM_REQ:
-	case NVME_TCP_PDU_TYPE_C2H_TERM_REQ:
-	case NVME_TCP_PDU_TYPE_CAPSULE_RESP:
-	case NVME_TCP_PDU_TYPE_R2T:
-		if (ch->pdo != 0) {
-			printf("NVMe/TCP: Invalid PDU data offset %u\n",
-			    ch->pdo);
-			nvmf_tcp_report_error(qp,
-			    NVME_TCP_TERM_REQ_FES_INVALID_HEADER_FIELD, 3, m,
-			    hlen);
-			return (EBADMSG);
-		}
-		break;
-	case NVME_TCP_PDU_TYPE_CAPSULE_CMD:
-	case NVME_TCP_PDU_TYPE_H2C_DATA:
-	case NVME_TCP_PDU_TYPE_C2H_DATA:
-		/* Permit PDO of 0 if there is no data. */
-		if (full_hlen == plen && ch->pdo == 0)
-			break;
-
-		if (ch->pdo < full_hlen || ch->pdo > plen ||
-		    ch->pdo % qp->rxpda != 0) {
-			printf("NVMe/TCP: Invalid PDU data offset %u\n",
-			    ch->pdo);
-			nvmf_tcp_report_error(qp,
-			    NVME_TCP_TERM_REQ_FES_INVALID_HEADER_FIELD, 3, m,
-			    hlen);
-			return (EBADMSG);
-		}
-		break;
-	}
-
-	/* Validate plen. */
-	if (plen < ch->hlen) {
-		printf("NVMe/TCP: Invalid PDU length %u\n", plen);
-		nvmf_tcp_report_error(qp,
-		    NVME_TCP_TERM_REQ_FES_INVALID_HEADER_FIELD, 4, m, hlen);
-		return (EBADMSG);
-	}
-
-	if (plen == full_hlen)
-		data_len = 0;
-	else
-		data_len = plen - ch->pdo;
-	switch (ch->pdu_type) {
-	default:
-		__assert_unreachable();
-		break;
-	case NVME_TCP_PDU_TYPE_H2C_TERM_REQ:
-	case NVME_TCP_PDU_TYPE_C2H_TERM_REQ:
-		/* Checked above. */
-		MPASS(plen <= NVME_TCP_TERM_REQ_PDU_MAX_SIZE);
-		break;
-	case NVME_TCP_PDU_TYPE_CAPSULE_CMD:
-	case NVME_TCP_PDU_TYPE_H2C_DATA:
-	case NVME_TCP_PDU_TYPE_C2H_DATA:
-		if ((ch->flags & NVME_TCP_CH_FLAGS_DDGSTF) != 0 &&
-		    data_len <= sizeof(rx_digest)) {
-			printf("NVMe/TCP: PDU %u too short for digest\n",
-			    ch->pdu_type);
-			nvmf_tcp_report_error(qp,
-			    NVME_TCP_TERM_REQ_FES_INVALID_HEADER_FIELD, 4, m,
-			    hlen);
-			return (EBADMSG);
-		}
-		break;
-	case NVME_TCP_PDU_TYPE_R2T:
-	case NVME_TCP_PDU_TYPE_CAPSULE_RESP:
-		if (data_len != 0) {
-			printf("NVMe/TCP: PDU %u with data length %u\n",
-			    ch->pdu_type, data_len);
-			nvmf_tcp_report_error(qp,
-			    NVME_TCP_TERM_REQ_FES_INVALID_HEADER_FIELD, 4, m,
-			    hlen);
-			return (EBADMSG);
-		}
-		break;
+	error = nvmf_tcp_validate_pdu_header(ch, qp->qp.nq_controller,
+	    qp->header_digests, qp->data_digests, qp->rxpda, &data_len, &fes,
+	    &fei);
+	if (error != 0) {
+		if (error != ECONNRESET)
+			nvmf_tcp_report_error(qp, fes, fei, m, hlen);
+		return (error);
 	}
 
 	/* Check header digest if present. */
@@ -580,7 +357,7 @@ nvmf_tcp_validate_pdu(struct nvmf_tcp_qpair *qp, struct nvmf_tcp_rxpdu *pdu)
 			printf("NVMe/TCP: Header digest mismatch\n");
 			nvmf_tcp_report_error(qp,
 			    NVME_TCP_TERM_REQ_FES_HDGST_ERROR, rx_digest, m,
-			    full_hlen);
+			    hlen);
 			return (EBADMSG);
 		}
 	}
@@ -588,7 +365,6 @@ nvmf_tcp_validate_pdu(struct nvmf_tcp_qpair *qp, struct nvmf_tcp_rxpdu *pdu)
 	/* Check data digest if present. */
 	pdu->data_digest_mismatch = false;
 	if ((ch->flags & NVME_TCP_CH_FLAGS_DDGSTF) != 0) {
-		data_len -= sizeof(rx_digest);
 		digest = mbuf_crc32c(m, ch->pdo, data_len);
 		m_copydata(m, plen - sizeof(rx_digest), sizeof(rx_digest),
 		    (caddr_t)&rx_digest);
