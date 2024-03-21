58519aa5f07a51e49a829fca3e3e75b74bd9fb7b
John Baldwin <jhb@FreeBSD.org>
2024-03-21 11:45:01 -0700

    nvmf_proto.h: Update to latest version of nvmf_spec.h

--- sys/dev/nvmf/nvmf_proto.h
+++ sys/dev/nvmf/nvmf_proto.h
@@ -1,34 +1,6 @@
-/*-
- *   BSD LICENSE
- *
- *   Copyright (c) Intel Corporation.
+/*   SPDX-License-Identifier: BSD-3-Clause
+ *   Copyright (C) 2016 Intel Corporation.
  *   All rights reserved.
- *
- *   Redistribution and use in source and binary forms, with or without
- *   modification, are permitted provided that the following conditions
- *   are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in
- *       the documentation and/or other materials provided with the
- *       distribution.
- *     * Neither the name of Intel Corporation nor the names of its
- *       contributors may be used to endorse or promote products derived
- *       from this software without specific prior written permission.
- *
- *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 /* Derived from include/spdk/nvmf_spec.h from Intel's SPDK. */
@@ -166,12 +138,21 @@ enum nvmf_adrfam {
  * NVM subsystem types
  */
 enum nvmf_subtype {
-	/** Discovery type for NVM subsystem */
+	/** Referral to a discovery service */
 	NVMF_SUBTYPE_DISCOVERY		= 0x1,
 
-	/** NVMe type for NVM subsystem */
+	/** NVM Subsystem */
 	NVMF_SUBTYPE_NVME		= 0x2,
+
+	/** Current Discovery Subsystem */
+	NVMF_SUBTYPE_DISCOVERY_CURRENT	= 0x3
 };
+
+/* Discovery Log Entry Flags - Duplicate Returned Information */
+#define NVMF_DISCOVERY_LOG_EFLAGS_DUPRETINFO (1u << 0u)
+
+/* Discovery Log Entry Flags - Explicit Persistent Connection Support for Discovery */
+#define NVMF_DISCOVERY_LOG_EFLAGS_EPCSD (1u << 1u)
 
 /**
  * Connections shall be made over a fabric secure channel
@@ -400,8 +381,11 @@ enum nvme_tcp_secure_socket_type {
 	/** No security */
 	NVME_TCP_SECURITY_NONE				= 0,
 
-	/** TLS (Secure Sockets) */
-	NVME_TCP_SECURITY_TLS				= 1,
+	/** TLS (Secure Sockets) version 1.2 */
+	NVME_TCP_SECURITY_TLS_1_2			= 1,
+
+	/** TLS (Secure Sockets) version 1.3 */
+	NVME_TCP_SECURITY_TLS_1_3			= 2,
 };
 
 /** TCP transport-specific address subtype */
@@ -459,7 +443,10 @@ struct nvmf_discovery_log_page_entry {
 	/** Admin max SQ size */
 	uint16_t	asqsz;
 
-	uint8_t		reserved0[22];
+	/** Entry Flags */
+	uint16_t	eflags;
+
+	uint8_t		reserved0[20];
 
 	/** Transport service identifier */
 	uint8_t		trsvcid[NVMF_TRSVCID_MAX_LEN];
@@ -617,8 +604,9 @@ _Static_assert(offsetof(struct nvme_tcp_ic_req, pfv) == 8, "Incorrect offset");
 _Static_assert(offsetof(struct nvme_tcp_ic_req, hpda) == 10, "Incorrect offset");
 _Static_assert(offsetof(struct nvme_tcp_ic_req, maxr2t) == 12, "Incorrect offset");
 
-#define NVME_TCP_CPDA_MAX		31
-#define NVME_TCP_PDU_PDO_MAX_OFFSET	((NVME_TCP_CPDA_MAX + 1) << 2)
+#define NVME_TCP_HPDA_MAX 31
+#define NVME_TCP_CPDA_MAX 31
+#define NVME_TCP_PDU_PDO_MAX_OFFSET     ((NVME_TCP_CPDA_MAX + 1) << 2)
 
 /**
  * ICResp
