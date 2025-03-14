e5b04c6ea94a01984abfe2b1c15e9770d2486333
John Baldwin <jhb@FreeBSD.org>
2025-03-14 14:49:47 -0400

    squash: Reimplement stringf using fwopen

--- lib/libutil++/stringf.cc
+++ lib/libutil++/stringf.cc
@@ -7,24 +7,41 @@
 
 #include <stdarg.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string>
 
 #include "libutil++"
+
+static int
+stringf_write(void *cookie, const char *buf, int len)
+{
+	std::string *str = reinterpret_cast<std::string *>(cookie);
+	try {
+		str->append(buf, len);
+	} catch (std::bad_alloc) {
+		errno = ENOMEM;
+		return (-1);
+	} catch (std::length_error) {
+		errno = EFBIG;
+		return (-1);
+	}
+	return (len);
+}
 
 std::string
 freebsd::stringf(const char *fmt, ...)
 {
 	va_list ap;
-	char *str;
+	std::string str;
+	freebsd::FILE_up fp(fwopen(reinterpret_cast<void *>(&str),
+	    stringf_write));
 
 	va_start(ap, fmt);
-	vasprintf(&str, fmt, ap);
+	vfprintf(fp.get(), fmt, ap);
 	va_end(ap);
-	if (str == NULL)
-		throw std::bad_alloc();
 
-	std::string res(str);
-	free(str);
-	return res;
+	if (ferror(fp.get()))
+		throw std::bad_alloc();
+	fp.reset(nullptr);
+
+	return str;
 }
